fc0f6c622a5e969e814d42e26878fbdddfafec24
Christian Foellmann <foellmann@foe-services.de>
2013-11-15 18:48:50 +0100

    change dthe .nuspec files to versions w/o version numbers. updated Graphwiz. updated PureText + added x64 version. Fixed link for Ransack

--- Graphviz/tools/chocolateyInstall.ps1
+++ Graphviz/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'Graphviz' 'msi' '/passive' 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.28.0.msi'
+Install-ChocolateyPackage 'Graphviz' 'msi' '/passive' 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.34.msi'
