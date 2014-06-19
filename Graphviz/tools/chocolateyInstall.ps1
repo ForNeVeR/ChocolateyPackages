affd7185b0ba5ffb798566455ed15e708397c63c
Anthony Mastrean <anthony.mastrean@gmail.com>
2014-06-19 16:34:24 -0400

    Use the standard MSI quiet switch

--- Graphviz/tools/chocolateyInstall.ps1
+++ Graphviz/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'Graphviz' 'msi' '/passive' 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.34.msi'
+Install-ChocolateyPackage 'Graphviz' 'msi' '/Q' 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.34.msi'
