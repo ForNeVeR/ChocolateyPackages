fc484a144c5b0bc916456e4280af4d2a5e1ed25c
Friedrich von Never <friedrich@fornever.me>
2015-01-02 14:06:01 +0600

    Update graphviz to 2.38.

--- Graphviz/tools/chocolateyInstall.ps1
+++ Graphviz/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'Graphviz' 'msi' '/Q' 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.34.msi'
+Install-ChocolateyPackage 'Graphviz' 'msi' '/Q' 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.38.msi'
