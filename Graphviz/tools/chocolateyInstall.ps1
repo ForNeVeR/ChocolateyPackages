09ca260af9be63fcc11fa3394b187a5456c3ffcc
Friedrich von Never <friedrich@fornever.me>
2016-08-30 21:22:43 +0700

    Graphviz: add SHA256 checksum

--- Graphviz/tools/chocolateyInstall.ps1
+++ Graphviz/tools/chocolateyInstall.ps1
@@ -1,1 +1,9 @@
-Install-ChocolateyPackage 'Graphviz' 'msi' '/Q' 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.38.msi'
+$sha256 = 'C794EA03BC2631FFF468F4D97FA6726C536FC98EE579529779AA6F45E94E4F6D'
+$url = 'http://www.graphviz.org/pub/graphviz/stable/windows/graphviz-2.38.msi'
+
+Install-ChocolateyPackage 'Graphviz' `
+    -FileType 'msi' `
+    -SilentArgs '/Q' `
+    -Checksum $sha256 `
+    -ChecksumType 'sha256' `
+    -Url $url
