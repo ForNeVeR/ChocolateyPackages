e75cd3cace4b7623b04dd614d850e49433643863
Friedrich von Never <friedrich@fornever.me>
2021-03-13 20:22:46 +0700

    CI: pack all packages

---
+++ scripts/pack-all.ps1
@@ -0,0 +1,18 @@
+﻿param (
+    $PackagesBase = "$PSScriptRoot/.."
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$packages = @(
+    'cask'
+)
+
+foreach ($package in $packages) {
+    Write-Output "Packaging $package…"
+    choco pack $PackagesBase/$package/$package.nuspec
+    if (!$?) {
+        throw "Choco pack for $package returned $LASTEXITCODE"
+    }
+}
